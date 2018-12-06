568f56f3fcdab8418479f233848abbef48649fbc
Truman Proctor <trumanproctor@Trumans-MacBook-Pro.local>
2018-12-05 21:53:15 -0700

    class work 181205

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bc = require('./controllers/books_controller');
+
+const app = express();
+
+app.use( bodyParser.json() );
+
+app.get('/api/books', bc.read);
+app.post('/api/books', bc.create);
+app.put('/api/books/:id', bc.update);
+app.delete('/api/books/:id', bc.delete);
+
+const port = 4000;
+app.listen( port, () => { console.log('Server listening on port ${port}');} );
